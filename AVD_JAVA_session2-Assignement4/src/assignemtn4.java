6b4640098c8287233204dfd3914d576d4a562b8f
u174762 <sam.panga@hotmail.com>
2016-08-07 21:35:11 +0530

    Add files via upload

---
+++ AVD_JAVA_session2-Assignement4/src/assignemtn4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class assignemtn4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			
+		Scanner sc=new Scanner(System.in); 
+			int month=sc.nextInt();
+	switch(month){
+	case 1: System.out.println("jan,31 days");break;
+	case 2: System.out.println("feb,28 days");break;
+	case 3: System.out.println("march,31 days");break;
+	case 4: System.out.println("april,30 days");break;
+	case 5: System.out.println("may,31 days");break;
+	case 6: System.out.println("june,30 days");break;
+	case 7: System.out.println("july,31 days");break;
+	case 8: System.out.println("aug,31days");break;
+	case 9: System.out.println("sep,30days");break;
+	case 10: System.out.println("oct,31 days");break;
+	case 11: System.out.println("nov,30 days");break;
+	case 12: System.out.println("dec,31days");break;
+	}
+		}
+
+	}
